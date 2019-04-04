7a3226fd2e1cfe85fb0c49f5bd477b9928bf534f
Tamilarasi-k <49061914+Tamilarasi-k@users.noreply.github.com>
2019-04-04 15:14:00 +0530

    graphql mutation data insert

---
+++ schemadb.js
@@ -0,0 +1,124 @@
+var graphql = require('graphql');
+var _ = require('lodash');// for filter
+var {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,GraphQLInt,GraphQLList} = graphql;
+
+const Book = require('../models/book');
+const Author = require('../models/author');
+
+// schema
+var BookType = new GraphQLObjectType({
+    name :'Book',
+    fields: ()=>({ // read rootquery so we use function
+        // parent
+      id:{type:GraphQLID},
+      name:{type:GraphQLString},
+      genre:{type:GraphQLString},
+      // map author
+      author:{
+          type:AuthorType,
+          resolve(parent,args){
+              // parent -- book
+             // return _.find(authors,{id:parent.authorId});
+          }
+      }
+    })
+});
+var AuthorType = new GraphQLObjectType({
+    name :'Author',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt},
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                //return _.filter(books,{authorId:parent.id});
+                // filter for multiple data
+            }
+        }    
+    })
+});
+var rootquery = new GraphQLObjectType({
+    name :'rootquery', // any name
+    fields:{
+        book :{
+            type:BookType,// link schema
+            // list of arguments
+            // to use map the id 
+            args:{
+                id:{type:GraphQLID}
+            },
+            // parent data
+            resolve(parent,args){
+            // db logic like mongodb
+            // use lodash for id filter
+           // return _.find(books,{id:args.id});
+
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{
+                id:{type:GraphQLID}
+            },
+            resolve(parent,args){
+               // return _.find(authors,{id:args.id});
+            }
+        },
+         // map books
+         books:{
+            // we shouldn't  use BookType because here books is liat so we need to use GraphQLList
+           type: new GraphQLList(BookType),// declare GraphQLList
+           resolve(parent,args){
+              // return books ; // books array
+           }
+       },
+      authors:{
+          type:new GraphQLList(AuthorType),
+          resolve(parent,args){
+              //return authors;  // authors array
+          }
+
+      }
+    }
+});
+
+const Muatation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+    }
+})
+module.exports = new GraphQLSchema({
+    query:rootquery,
+    mutation: Muatation
+});
