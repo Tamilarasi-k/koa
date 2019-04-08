7aa08e2c12c361e817068b9fe365817942c9a46f
Tamilarasi-k <49061914+Tamilarasi-k@users.noreply.github.com>
2019-04-08 15:26:14 +0530

    graphql

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 var koa = require('koa');
-var mount = require('koa-mount');
+var mount = require("koa-mount");
 var graphqlHTTP = require('koa-graphql');
 var app =new koa();
 var schema =require('./schema/schema');
-// database connection
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://192.168.1.7:27017/my_db', { useNewUrlParser: true });
+ mongoose.connection.once('open',()=>{
+     console.log("connected to database")
+ })
+app.use(mount('/graphql',graphqlHTTP({
+ schema,
+ graphiql: true
+})))
 
-    mongoose.connect('mongodb://192.168.1.6/my_db', { useNewUrlParser: true });
-    // mongoose.connect.once('open',()=>{
-    //    console.log("connected to database");
-    // });
-    app.use(mount('/vendor',graphqlHTTP( {
-        graphiql:true,
-        schema
-
-    })))
-
-
-app.listen(3000,()=>
-{
-    console.log("Server running.....")
-})
+app.listen(3000, () => {
+    console.log("Server is Running.....");
+});
